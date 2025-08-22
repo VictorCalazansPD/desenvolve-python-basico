2ca75ff111257af36c9bc7517ab04b183b537df3
VictorCalazansPD <victor.lage@pditabira.com>
2025-08-21 22:31:31 -0300

    Update aula2_questaoX.py

--- aula2_questaoX.py
+++ aula2_questaoX.py
@@ -1,4 +1,4 @@
 1 - https://live.staticflickr.com/65535/54736568419_452b895ea5_b.jpg
-2 - https://live.staticflickr.com/65535/54736336481_5acb7b1d6e_b.jpg3
+2 - https://live.staticflickr.com/65535/54736336481_5acb7b1d6e_b.jpg
 3 - https://live.staticflickr.com/65535/54736705835_e8427911f3_b.jpg
 4 - https://live.staticflickr.com/65535/54736344231_174527c559_b.jpg
